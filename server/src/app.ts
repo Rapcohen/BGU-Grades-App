c215600044011639a71782333d207ab3bfc4e912
Raphael <rapcohen@gmail.com>
2022-03-05 19:34:22 +0200

    Prepare for deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,15 @@
 import express, { Application } from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app: Application = express();
 
 routes(app);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 // Start the server
-const PORT: number = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
