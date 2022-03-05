307ab1eed25b819db7cd78bd3fa50454d8c023dc
Raphael <rapcohen@gmail.com>
2022-03-05 12:00:00 +0200

    Added MUI, Formik & changed form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,34 @@
 import React from "react";
+import { GradeForm } from "./components/GradeForm";
+import GradeFormValues from "./interfaces/GradeFormValues";
 import "./App.css";
-import Form from "./components/Form";
 
-interface AppProps {}
+interface AppProps { }
+
+async function getGradesFromBGU(values: GradeFormValues): Promise<void> {
+  try {
+    const requestUrl = `/grades/${values.year}/${values.semester}/${values.courseId}`;
+    const response: Response = await fetch(requestUrl);
+
+    if (response.ok) {
+      const pdfData: string = await response.text();
+      const blob: Blob = new Blob([pdfData], { type: "application/pdf" });
+      const url: string = URL.createObjectURL(blob);
+      window.open(url, "_blank");
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const App: React.FC<AppProps> = () => {
   return (
     <div className="app-container">
-      <h1>BGU Grades</h1>
-      <Form />
+      <div className="form-container">
+        <h1>BGU Grades</h1>
+        <GradeForm onSubmit={getGradesFromBGU} />
+      </div>
     </div>
   );
 };
---
+++ client/src/interfaces/GradeFormValues.ts
@@ -0,0 +1,7 @@
+interface GradeFormValues {
+    courseId: string;
+    year: string;
+    semester: string;
+}
+
+export default GradeFormValues;
