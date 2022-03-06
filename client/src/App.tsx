0da63ba4ed52709ce6c0cc547be5ae9263795ece
Amir Zaushnizer <78536626+amirzaushnizer@users.noreply.github.com>
2022-03-06 21:37:41 +0200

    Small code improvements (#1)
    * some small improvements
    
    * some small improvements
    
    * fix
    
    Co-authored-by: amirzaushnizer <amir.zaushnizer@duda.co>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,27 @@
 import React from "react";
 import { GradeForm } from "./components/GradeForm";
-import GradeFormValues from "./interfaces/GradeFormValues";
 import { Grid } from "@mui/material";
 import "./App.css";
 
 interface AppProps {}
 
-async function getGradesFromBGU(values: GradeFormValues): Promise<void> {
-	try {
-		const requestUrl = `/grades/${values.year}/${values.semester}/${values.courseId}`;
-		const response: Response = await fetch(requestUrl);
-
-		if (response.ok) {
-			const pdfData: string = await response.text();
-			const blob: Blob = new Blob([pdfData], { type: "application/pdf" });
-			const url: string = URL.createObjectURL(blob);
-			window.open(url, "_blank");
-			return;
-		}
-	} catch (error) {
-		console.log(error);
-	}
-}
-
 const App: React.FC<AppProps> = () => {
-	return (
-		<Grid
-			container
-			direction="column"
-			alignContent="center"
-			justifyContent="center"
-			style={{ height: "100vh" }}
-		>
-			<Grid item textAlign="center">
-				<h1>BGU Grades</h1>
-			</Grid>
-			<Grid item>
-				<GradeForm onSubmit={getGradesFromBGU} />
-			</Grid>
-		</Grid>
-	);
+  return (
+    <Grid
+      container
+      direction="column"
+      alignContent="center"
+      justifyContent="center"
+      style={{ height: "100vh" }}
+    >
+      <Grid item textAlign="center">
+        <h1>BGU Grades</h1>
+      </Grid>
+      <Grid item>
+        <GradeForm />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default App;
